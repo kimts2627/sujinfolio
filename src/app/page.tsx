402ab9527aa5b1f316d3538938d4dcc7952e263f
kimts2627 <like.gecko1@gmail.com>
2023-08-26 18:10:22 +0900

    feat: 메인 페이지의 vertical 전용 사진을 추가 함
    Signed-off-by: aaron <like.gecko1@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,41 +4,16 @@ import { useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-import dummy1 from "public/images/dummy_1.jpeg";
-import dummy2 from "public/images/dummy_2.jpeg";
-import dummy3 from "public/images/dummy_3.jpeg";
-import dummy4 from "public/images/dummy_4.jpeg";
+import cards from "../../models/card.json";
+import { Card } from "../../types";
+
+const IMAGE_BASE_PATH = "/images";
 
 export default function Home() {
   const router = useRouter();
   const [highlightIndex, setHighlightIndex] = useState(0);
 
-  const previewCards = [
-    {
-      id: 1,
-      title: "mock",
-      description: "mock mock",
-      src: dummy1,
-    },
-    {
-      id: 2,
-      title: "mock",
-      description: "mock mock",
-      src: dummy2,
-    },
-    {
-      id: 3,
-      title: "mock",
-      description: "mock mock",
-      src: dummy3,
-    },
-    {
-      id: 4,
-      title: "mock",
-      description: "mock mock",
-      src: dummy4,
-    },
-  ];
+  const previewCards: Card[] = cards.slice(0, 5);
 
   const handleCardClick = (cardId: number) => {
     router.push(`/gallery/${cardId}`);
@@ -46,10 +21,10 @@ export default function Home() {
 
   return (
     <main className="flex justify-center w-full h-full">
-      <ul className="flex gap-2 w-full h-full">
+      <ul className="flex gap-5 w-full h-full">
         {previewCards.map((card, index) => (
           <li
-            className={`h-full overflow-hidden cursor-pointer ease-in-out duration-700 ${
+            className={`h-full overflow-hidden cursor-pointer ease-in-out duration-0 ${
               highlightIndex === index
                 ? "brightness-100"
                 : "flex-1 brightness-50"
@@ -59,10 +34,11 @@ export default function Home() {
             key={card.id}
           >
             <Image
-              src={card.src}
+              src={(IMAGE_BASE_PATH + card.images.vertical) as string}
               alt={card.title + " preview"}
-              className={`w-full h-full object-cover object-position-${card.id}`}
-              objectFit="cover"
+              className={`w-full h-full object-cover `}
+              width={350}
+              height={1000}
             />
           </li>
         ))}
