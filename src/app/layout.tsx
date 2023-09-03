6be585874bc6a3d4cf49dc23b24938ab12026054
kimts2627 <like.gecko1@gmail.com>
2023-09-03 19:17:51 +0900

    chore: GA 수집을 위한 설정 추가
    Signed-off-by: aaron <like.gecko1@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
+
 import Header from "./Header";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,6 +19,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-047STP9YZK" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-047STP9YZK');
+        `}
+        </Script>
+      </head>
+
       <body className={inter.className + " flex items-center justify-center"}>
         <div className="flex h-screen w-screen max-w-8xl border-x border-solid border-black bg-white">
           <Header />
